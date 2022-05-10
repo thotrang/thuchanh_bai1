e8d23db932376a3244138917de52e78d5e49ff03
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-10 15:34:58 +0700

    keydown

--- event-/event-key/main.js
+++ event-/event-key/main.js
@@ -1,53 +1,33 @@
-function topDirection(){
-    let anh=document.getElementById(myImage);
-    anh.style.top=parseInt(anh.style.top) + 5+ 'px';
+function topDirection() {
+    let anh = document.getElementById('myImage');
+    anh.style.top = parseInt(anh.style.top) - 5 + 'px';
 }
-function bottomDirection(){
-    let anh=document.getElementById(myImage);
-    anh.style.top=parseInt(anh.style.top) - 5+ 'px';
+function bottomDirection() {
+    let anh = document.getElementById('myImage');
+    anh.style.top = parseInt(anh.style.top) + 5 + 'px';
 }
-function leftDirection(){
-    let anh=document.getElementById(myImage);
-    anh.style.left=parseInt(anh.style.left) + 5+ 'px';
+function leftDirection() {
+    let anh = document.getElementById('myImage');
+    anh.style.left = parseInt(anh.style.left) - 5 + 'px';
 }
-function rightDirection(){
-    let anh=document.getElementById(myImage);
-    anh.style.left=parseInt(anh.style.left) - 5+ 'px';
+function rightDirection() {
+    let anh = document.getElementById('myImage');
+    anh.style.left = parseInt(anh.style.left) + 5 + 'px';
 }
-function myDirection(huong){
-    switch(huong.keyCode){
+function myDirection(event) {
+    switch (event.keyCode) {
         case 37:
             leftDirection();
             break;
         case 38:
-            rightDirection();
+            topDirection();
             break;
         case 39:
-            topDirection();
+            rightDirection();
             break;
         case 40:
             bottomDirection();
             break;
     }
-    
+
 }
-myDirection();
-// function moveSelection(evt) {
-//     switch (evt.keyCode) {
-//         case 37:
-//             leftArrowPressed();
-//             break;
-//         case 39:
-//             rightArrowPressed();
-//             break;
-//         case 38:
-//             upArrowPressed();
-//             break;
-//         case 40:
-//             downArrowPressed();
-//             break;
-//     }
-// }
-// function docReady() {
-//     window.addEventListener('keydown', moveSelection);
-// }
