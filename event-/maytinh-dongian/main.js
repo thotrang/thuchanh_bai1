0457a0e207879c9c0eaa2004add0164edcf76bcc
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-10 10:18:05 +0700

    may tính đơn giản

--- event-/maytinh-dongian/main.js
+++ event-/maytinh-dongian/main.js
@@ -1,6 +1,15 @@
-
-function tinhToan(){
-
+function nhap(value){
+    
+    let s = document.getElementById("number").value;
+    document.getElementById("number").value = s + value;
 }
-var heightBrowser = window.innerHeight;
-alert(heightBrowser);
+function tinhToan(){
+    let s =document.getElementById('number').value;
+    
+    document.getElementById("number").value=eval(s);
+}
+//nhấn nút để giá trị đc đưa lên ô text
+// bắt sự kiện cho nút ấn vào>
+// gọi giá trị trên text xuống để dùng eval để tính toán
+// in ra kết quả trên text
+// reset
